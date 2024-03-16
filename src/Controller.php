3b00d8a36303c05ae2d8e6d6a15d7581822f2152
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2024-03-16 15:46:06 -0300

    Ajuste componente de mensagens

--- src/Controller.php
+++ src/Controller.php
@@ -29,67 +29,56 @@ abstract class Controller {
         if(isset($_GET['error'])) {
             if($_GET['error'] === "invalid-fields") {
                 $this->message = "Preencha todos os campos";
-                $this->class = "danger";
             }
             if($_GET['error'] === "invalid-auth") {
                 $this->message = "E-mail ou senha inválido(os)";
-                $this->class = "danger";
+                
             }
-
             if($_GET['error'] === "call-not-found") {
                 $this->message = "Chamado não encontrado";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "different-passwords") {
                 $this->message = "As senhas devem ser iguais";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "used-email") {
                 $this->message = "O e-mail já está em uso!";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "not-allowed") {
                 $this->message = "Tente novamente mais tarde";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "confirm-not-allowed") {
                 $this->message = "Tente novamente mais tarde";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "invalid-password-lenght") {
                 $this->message = "A senha deve ter pelo menos 8 caracteres!";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "call-not-found") {
                 $this->message = "Chamado não encontrado!";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "invalid-old-pass") {
                 $this->message = "Senha antiga incorreta!";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "email-not-found") {
                 $this->message = "O e-mail informado não foi encontrado!";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "user-deleted") {
                 $this->message = "Usuário deletado com sucesso";
-                $this->class = "danger";
             }
 
             if($_GET['error'] === "file-not-imported") {
                 $this->message = "O arquivo não pôde ser importado!";
-                $this->class = "danger";
             }
+            
+            $this->class = "danger";
         }  
     }
 
@@ -97,8 +86,8 @@ abstract class Controller {
         if(isset($_GET['info'])) {
             if($_GET['info'] === "logout") {
                 $this->message = "Até mais!";
-                $this->class = "primary";
             }
+            $this->class = "primary";
         }  
     }
 
@@ -106,8 +95,8 @@ abstract class Controller {
         if(isset($_GET['warning'])) {
             if($_GET['warning'] === "user-not-confirmed") {
                 $this->message = "Confirme seu e-mail para logar";
-                $this->class = "warning";
             }
+            $this->class = "warning";
         } 
     }
 
@@ -115,48 +104,40 @@ abstract class Controller {
         if(isset($_GET['success'])) {
             if($_GET['success'] === "call-created") {
                 $this->message = "Atendimento criado";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "user-created") {
                 $this->message = "Confirme seu e-mail para continuar";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "call-deleted") {
                 $this->message = "Chamado deletado!";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "call-updated") {
                 $this->message = "Chamado atualizado!";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "pass-updated") {
                 $this->message = "Senha Atualizada!";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "system-created") {
                 $this->message = "Sistema criado!";
-                $this->class = "success";
             }
             
             if($_GET['success'] === "user-admin-created") {
                 $this->message = "Usuário criado!";
-                $this->class = "success";
             }
 
             if($_GET['success'] === "file-imported") {
                 $this->message = "Arquivo Importado com sucesso!";
-                $this->class = "success";
             }
 
             if($_GET['success'] === "email-send") {
                 $this->message = "Email enviado com sucesso";
-                $this->class = "success";
             }
+            $this->class = "success";
         }  
     }
 }
